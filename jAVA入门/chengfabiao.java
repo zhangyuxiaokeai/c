ba338d90d568b89d1790428c681335633522761f
zy <1464495600@qq.com>
2022-08-28 23:41:54 +0800

    java

---
+++ jAVA入门/chengfabiao.java
@@ -0,0 +1,14 @@
+class   VariableTest
+{
+	int i=0;
+	int j=0;
+	for(i=1;i<=9;i++)
+	{
+		for(j=1;j<=i;j++)
+		{
+			System.out.println(i+"*"+j+"="+(i*j)+("\t"));
+		}
+			System.out.println();
+	}
+	}
+}
